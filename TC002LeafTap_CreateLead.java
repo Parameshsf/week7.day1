0841bc0063f7420dbb506f6223b6c170327d7617
Parameshsf <91655090+Parameshsf@users.noreply.github.com>
2021-11-15 08:13:39 +0530

    Add files via upload

--- TC002LeafTap_CreateLead.java
+++ TC002LeafTap_CreateLead.java
@@ -18,7 +18,11 @@ public class TC002LeafTap_CreateLead extends LeafTapCommonMethods {
 	@Test(dataProvider = "readExcel") //readExcel is the method name used for DataProvider
 	//We can give like the above only when DataProvider(indices = 0) used in the DataProvider declaration
 	//@Test(dataProvider = "createLead")
-	public void verifyCreateLead(String compName) throws InterruptedException {
+	public void verifyCreateLead(String compName, String fName, String lName, String source, String mktCamp,
+			String fNameLocal, String lNameLocal, String title, String dept, String Industry,
+			String ownership, String desc, String impNote, String empCount, String countryCode,
+			String areaCode, String phNo, String email, String country, String state, String city,
+			String postalCode) throws InterruptedException {
 		// TODO Auto-generated method stub
 		LeafTap_LoginPage lp = new LeafTap_LoginPage(driver);//Passing driver as an argument for constructor
 		lp.enterUserName("Demosalesmanager")
@@ -28,9 +32,30 @@ public class TC002LeafTap_CreateLead extends LeafTapCommonMethods {
 		.clickCRMSFALink()
 		.clickLeadsTab()
 		.clickleadsLink()
-		.enterCompName(compName);
-	
-
+		.enterCompName(compName)
+		.enterFirstName(fName)
+		.enterLastName(lName)
+		.selectSource(source)
+		.selectMarketingCampaign(mktCamp)
+		.enterFirstNameLocal(fNameLocal)
+		.enterLastNameLocal(lNameLocal)
+		.enterTitle(title)
+		.enterDepartment(dept)
+		.selectIndustry(Industry)
+		.selectOwnership(ownership)
+		.enterDescription(desc)
+		.enterImportantNote(impNote)
+		.enterNumberofEmp(empCount)
+		.enterCountryCode(countryCode)
+		.enterAreaCode(areaCode)
+		.enterPhoneNumber(phNo)
+		.enterEmail(email)
+		.selectCountry(country)
+		.selectState(state)
+		.enterCity(city)
+		.enterPostalCode(postalCode)
+		.clickCreateLeadButton();
+		
 	}
 
 }
